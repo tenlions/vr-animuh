87bc30abf1ee8d9318cbd37a4e839d3572535db6
nloewes <nicolas.loewes@serviceware.de>
2024-01-28 00:02:55 +0100

    Add baseline interfaces for abilities, implement punch ability

---
+++ Assets/Scripts/Abilities/AbilityColor.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AbilityColor
+{
+    Red,
+    Blue,
+    Green,
+    Yellow
+    
+}
---
+++ Assets/Scripts/Abilities/I_Hittable.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// interface for objects that can be hit by abilities
+public interface IHittable
+{
+    // called when the object is hit by an ability
+    public void Hit(AbilityHit hit);
+}
