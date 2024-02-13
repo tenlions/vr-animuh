539b95b18347c5764d4348c9c683c912abb1fc59
UncleSatchmo <157912114+UncleSatchmo@users.noreply.github.com>
2024-02-13 21:30:49 +0100

    vrf-20: clean up

--- Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -105,6 +105,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Vector2 MoveAxis
+        {
+            get
+            {
+                return SteamVR_Actions.default_MoveAxis;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
