64bf4ed539fe06ffafaa9ba03e00a5ddb998d5c3
nloewes <nicolas.loewes@serviceware.de>
2024-03-29 21:21:21 +0100

    vrf-22: fix hand direction detection

--- Assets/Scripts/Hands/AbilityColorSelector.cs
+++ Assets/Scripts/Hands/AbilityColorSelector.cs
@@ -1,14 +1,21 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Rendering.Universal.Internal;
 using Valve.VR.InteractionSystem;
+using Vector3 = UnityEngine.Vector3;
 
 // Offers methods to select an ability color 
 public class AbilityColorSelector : MonoBehaviour
 {
 
     private AbilityColor currentColor;
-    public float threshold = 0.00f;
+
+    private static readonly Vector3 V3_DEFAULT = new Vector3(666, 666, 666);
+    private Vector3 point_forward_pressed = V3_DEFAULT;
+    private const int IDX_X = 0;
+    private const int IDX_Z = 2;
 
     // Start is called before the first frame update
     void Start()
@@ -25,184 +32,59 @@ public class AbilityColorSelector : MonoBehaviour
     // Selects an ability color based on the hand's direction
     public AbilityColor SelectColor(Hand hand)
     {
-        HandDirection direction = GetHandDirection(hand);
+        // only select a color if we have usable values
+        if (point_forward_pressed == V3_DEFAULT) return currentColor;
+        
+        // get the vector between the pressed and released point
+        Vector3 between = point_forward_pressed - GetHandForward(hand);
+        int highestIndex = GetHighestValueIndex(between);
+        float highestValue = between[highestIndex];
 
-        switch (direction)
+        // if the highest value is on the x component and we are the left hand, invert value
+        if (hand.handType == Valve.VR.SteamVR_Input_Sources.LeftHand && highestIndex == IDX_X) highestValue = -highestValue;
+
+        Debug.Log("highest comp: " + (highestIndex == IDX_X ? "X" : "Z"));
+        Debug.Log("highest value: " + highestValue);
+
+        // reset the pressed point
+        point_forward_pressed = V3_DEFAULT;
+
+        // and return the color
+        switch (highestIndex)
         {
-            case HandDirection.UP:
-                return AbilityColor.Red;
-            case HandDirection.OUTWARD:
-                return AbilityColor.Grey;
-            case HandDirection.DOWN:
-                return AbilityColor.Blue;
-            case HandDirection.INWARD:
-                return AbilityColor.Yellow;
+            case IDX_X:
+                if (highestValue > 0) return AbilityColor.Red; // inward
+                else return AbilityColor.Grey; // outward
+            case IDX_Z:
+                if (highestValue > 0) return AbilityColor.Green;
+                else return AbilityColor.Blue;
             default:
                 return currentColor;
         }
     }
 
-    private struct DirectionAngles
+    public void SetForwardPressed(Hand hand)
     {
-
+        this.point_forward_pressed = GetHandForward(hand);
     }
 
-    private Vector3 OriginToVelocity(Vector3 velocity) 
+    // Returns the "forward" direction of the hand. Forward is the hand's forward vector rotated 45 degrees around the hand's right vector
+    private Vector3 GetHandForward(Hand hand)
     {
-        return velocity - transform.position;
+        return Quaternion.AngleAxis(45, hand.transform.right) * hand.transform.forward;
     }
 
-    private float AngleBetween(Vector3 velocity, Vector3 direction) 
+    private int GetHighestValueIndex(Vector3 vector)
     {
-        return Vector3.Angle(velocity, direction);
-    }
+        float absX = Mathf.Abs(vector.x);
+        float absZ = Mathf.Abs(vector.z);
 
-    private NonRelativeDirection LowestAngle(Vector3 velocity)
-    {
-        Dictionary<NonRelativeDirection, float> angles = new Dictionary<NonRelativeDirection, float>()
-        {
-            {NonRelativeDirection.UP, AngleBetween(OriginToVelocity(velocity), DirVec(transform.up)) },
-            {NonRelativeDirection.DOWN, AngleBetween(OriginToVelocity(velocity), DirVec(-transform.up)) },
-            {NonRelativeDirection.LEFT, AngleBetween(OriginToVelocity(velocity), DirVec(-transform.right)) },
-            {NonRelativeDirection.RIGHT, AngleBetween(OriginToVelocity(velocity), DirVec(transform.right)) },
-        };
-
-        return angles.OrderBy(e => e.Value).First().Key;
-    }
-
-    private Vector3 DirVec(Vector3 direction) 
-    {
-        return transform.position + direction;
-    }
-
-    // Returns the direction of the hand
-    private HandDirection GetHandDirection(Hand hand)
-    {
-        Vector3 vec3_velocity = hand.GetTrackedObjectVelocity();
-
-        if (true)
-        {
-
-            NonRelativeDirection dir = LowestAngle(vec3_velocity);
-            if (NonRelativeDirection.LEFT == dir)
-            {
-                Debug.Log("LEFT");
-                return HandDirection.INWARD;
-            }
-
-            if (NonRelativeDirection.RIGHT == dir)
-            {
-                Debug.Log("RIGHT");
-                return HandDirection.OUTWARD;
-            }
-
-            if (NonRelativeDirection.UP == dir)
-            {
-                Debug.Log("UP");
-                return HandDirection.UP;
-            }
-
-            if (NonRelativeDirection.DOWN == dir)
-            {
-                Debug.Log("DOWN");
-                return HandDirection.DOWN;
-            }
-
-            return HandDirection.NONE;
-        }
-
-        // only select a color if the hand is moving fast enough
-        if (vec3_velocity.magnitude < threshold)
-        {
-            return HandDirection.NONE;
-        }
-
-        Vector3 handForward = hand.transform.up;
-
-        float angle = Vector3.Angle(handForward, vec3_velocity);
-        Debug.Log("-----------------------------------------------");
-        Debug.Log("Forward: " + handForward);
-        Debug.Log("Velocity: " + vec3_velocity);
-        Debug.Log("Magnitude: " + vec3_velocity.magnitude);
-        Debug.Log("Angle: " + angle);
-        Debug.Log("-----------------------------------------------");
-
-        if (angle < 45f)
-        {
-            Debug.Log("UP UP UP");
-            return HandDirection.UP;
-        }
-        else if (angle < 135f)
-        {
-            Debug.Log("RIGHT RIGHT RIGHT");
-            return MapHorizontalDirection(hand, NonRelativeDirection.RIGHT);
-        }
-        else if (angle < 225f)
-        {
-            Debug.Log("DOWN DOWN DOWN");
-            return HandDirection.DOWN;
-        }
-        else
-        {
-            Debug.Log("LEFT LEFT LEFT");
-            return MapHorizontalDirection(hand, NonRelativeDirection.LEFT);
-        }
-    }
-
-    // Maps the given horizontal direction to the relative hand direction, meaning a left hand going left goes outward while a right hand going left goes inward
-    private HandDirection MapHorizontalDirection(Hand hand, NonRelativeDirection direction)
-    {
-        if (hand.handType == Valve.VR.SteamVR_Input_Sources.RightHand)
-        {
-            if (direction == NonRelativeDirection.RIGHT)
-            {
-                return HandDirection.OUTWARD;
-            }
-            else
-            {
-                return HandDirection.INWARD;
-            }
-        }
-        else
-        {
-            if (direction == NonRelativeDirection.RIGHT)
-            {
-                return HandDirection.INWARD;
-            }
-            else
-            {
-                return HandDirection.OUTWARD;
-            }
-        }
+        return absX > absZ ? IDX_X : IDX_Z;
     }
 
     // Returns the current ability color
     public AbilityColor GetCurrentColor()
     {
         return currentColor;
-    }
-
-    // the minimum velocity required to select a color
-    public float GetThreshold()
-    {
-        return threshold;
-    }
-
-    // Sets the minimum velocity required to select a color
-    public void SetThreshold(float threshold)
-    {
-        this.threshold = threshold;
-    }
-
-    // Global directions
-    private enum NonRelativeDirection 
-    { 
-        LEFT, RIGHT, UP, DOWN 
-    }
-
-    // Relative hand directions
-    private enum HandDirection
-    {
-        UP, DOWN, INWARD, OUTWARD, NONE
     }
 }
